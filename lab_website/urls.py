c977d3365171f25bf75e3e08b5238a52a41dc15a
Dave Bridges <dave.bridges@gmail.com>
2012-07-14 12:29:35 -0400

    Added sitemap framework and sitemaps for papers and personnel

--- lab_website/urls.py
+++ lab_website/urls.py
@@ -1,21 +1,26 @@
 '''This package has the url encodings for the main app.'''
-
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+from personnel.sitemap import LabPersonnelSitemap
+from papers.sitemap import LabPublicationsSitemap
+
+#this dictionary lists sitemap files which will be generated.
+sitemaps = {
+    'personnel': LabPersonnelSitemap,
+    'papers': LabPublicationsSitemap
+    }
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'lab_website.views.home', name='home'),
-    # url(r'^lab_website/', include('lab_website.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^communication/', include('communication.urls')), 
-    url(r'^papers/', include('papers.urls')),     
-)
+    url(r'^papers/', include('papers.urls')),
+    (r'^sitemap\.xml$', 'django.contrib.sitemaps.views.index', {'sitemaps': sitemaps}),
+    (r'^sitemap-(?P<section>.+)\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
+    )   
+
---
+++ papers/sitemap.py
@@ -0,0 +1,24 @@
+'''This package controls the sitemap for the papers app. 
+
+This sitemap will be generated at /sitemap-papers.xml
+'''
+
+from django.contrib.sitemaps import Sitemap
+
+from papers.models import Publication
+
+class LabPublicationsSitemap(Sitemap):
+    '''This sitemap lists all papers to be indexed.
+    
+    It only includes papers from this laboratory (laboratory_papers=True)'''
+
+    changefreq = 'yearly'
+    priority = '0.7'
+    
+    def items(self):
+        '''Filters Publications to show only laboratory papers.'''
+        return Publications.objects.filter(laboratory_papers=True)
+        
+    def lastmod(self, paper):
+        '''lastmod uses the last modification of the paper (not the comments).'''
+        return paper.date_last_modified
