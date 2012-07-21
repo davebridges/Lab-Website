a3fcf23d5279f102e8ed24ac28e3e27ea48f7cfd
Dave Bridges <dave.bridges@gmail.com>
2012-07-21 11:40:57 -0400

    Updated PublicationModelTests for new fixtures as part of issue #5

--- papers/tests.py
+++ papers/tests.py
@@ -14,7 +14,7 @@ MODELS = [Publication, AuthorDetails]
 
 class PublicationModelTests(TestCase):
     '''This class tests varios aspects of the `::class:Publication` model.'''
-    fixtures = ['fixture_publication']
+    fixtures = ['fixture_publication', 'fixture_publication_personnel']
 
     def setUp(self):
         '''Instantiate the test client.  Creates a test user.'''
@@ -35,23 +35,23 @@ class PublicationModelTests(TestCase):
                 
     def test_create_new_paper_minimum(self):
         '''This test creates a `::class:Publication` with the required information only.'''
-        test_publication = Publication(title='Test Publication')
+        test_publication = Publication(title='Test Publication.')
         test_publication.save()
+        self.assertEqual(test_publication.pk, 2)
         
-    def test_create_new_paper_all(self):
-        '''This test creates a `::class:Publication` with the required information only.'''
-        test_publication = Publication(title='Test Publication') #add more fields
-        test_publication.save()        
+    #def test_create_new_paper_all(self):
+        #'''This test creates a `::class:Publication` with the required information only.'''
+        #test_publication = Publication(title='Test Publication') #add more fields
+        #test_publication.save()        
         
     def test_paper_unicode(self):
         '''This tests the unicode representation of a `::class:Publication`.'''
-        test_publication = Publication(title='Test Publication')
-        test_publication.save()
-        self.assertEqual(test_publication.__unicode__(), "Test Publication")
+        test_publication = Publication.objects.get(title_slug='14-3-3-proteins-a-number-of-functions-for-a-numbered-protein')
+        self.assertEqual(test_publication.__unicode__(), "14-3-3 proteins: a number of functions for a numbered protein.")
         
     def test_paper_title_slug(self):
         '''This tests the title_slug field of a `::class:Publication`.'''
-        test_publication = Publication(title='Test Publication')
+        test_publication = Publication(title='Test Publication.')
         test_publication.save()
         self.assertEqual(test_publication.title_slug, "test-publication")  
         
@@ -63,9 +63,8 @@ class PublicationModelTests(TestCase):
      
     def test_paper_doi_link(self):
         '''This tests the title_slug field of a `::class:Publication`.'''
-        test_publication = Publication(title='Test Publication', doi='10.1126/stke.2422004re10')
-        test_publication.save()
-        self.assertEqual(test_publication.doi_link(), "http://dx.doi.org/10.1126/stke.2422004re10")                       
+        test_publication = Publication.objects.get(title="14-3-3 proteins: a number of functions for a numbered protein.")
+        self.assertEqual(test_publication.doi_link(), "http://dx.doi.org/10.1126/stke.2962005re10")                       
                     
 class AuthorDetailsModelTests(TestCase):
     '''This class tests varios aspects of the `::class:AuthorDetails` model.'''
