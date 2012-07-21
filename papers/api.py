531a3927dc77d0362b568e7463942cb959e9bcdb
Dave Bridges <dave.bridges@gmail.com>
2012-07-21 13:11:13 -0400

    Added allowed methods and filtering to papers API.  Part of issue #6.

--- papers/api.py
+++ papers/api.py
@@ -6,12 +6,20 @@ from tastypie.resources import ModelResource
 
 from papers.models import Publication
 
-
 class PublicationResource(ModelResource):
     '''This generates the API resource for Publications.
     
-    It returns all publications in the database'''
+    It returns all publications in the database.
+    Authors are currently not linked, as that would require an API to personnel.'''
     class Meta:
         '''The Meta class sets the conditions for the API.'''
         queryset = Publication.objects.all()
         resource_name = 'publications'
+        list_allowed_methods = ['get']
+        detail_allowed_methods = ['get'] 
+        include_absolute_url = True
+        filtering = {
+            "year": 'exact',
+            "type": ('exact', 'contains',),
+        }      
+               
