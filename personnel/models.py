be5b5520b365ee899913aaec9f4773e150e11043
Dave Bridges <dave.bridges@gmail.com>
2012-08-18 10:41:59 -0400

    Set Person model to be the UserProfile model

--- personnel/models.py
+++ personnel/models.py
@@ -2,6 +2,8 @@
 
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 GENDER_CHOICES = (
     ('M', 'Male'),
@@ -19,17 +21,18 @@ class Person(models.Model):
     '''This class describes laboratory members.
     
     This class will include current and former laboratory members.
-    
+    There are no required fields since this object can be created with a User instance creation.  
+    This is the UserProfile model and can be accessed from request.User.get_profile().
     The basis for this model is the http://schema.org/Person markup.'''
 	#these fields are for identification
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100, blank=True, null=True)
+    last_name = models.CharField(max_length=100, blank=True, null=True)
     additional_names = models.CharField(max_length=100, null=True, blank=True, help_text='other (i.e. middle) names')
     email = models.EmailField(blank=True, null=True)
     phone = models.CharField(max_length=40, blank=True, null=True)
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True, null=True)
     website = models.URLField(help_text='Personal Website', null=True, blank=True)
-    #image = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True, null=True)
+    image = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True, null=True)
     degrees = models.ManyToManyField('Degree', help_text="Graduate and Undergraduate Degrees", blank=True, null=True)
     awards = models.ManyToManyField('Award', blank=True, null=True)
     birthdate = models.DateField(blank=True, null=True)
@@ -43,7 +46,8 @@ class Person(models.Model):
     #these fields describe updating information and are automatically filled
     created = models.DateField(auto_now_add=True)
     updated = models.DateField(auto_now=True)
-    name_slug = models.SlugField(unique=True, editable=False)
+    name_slug = models.SlugField(editable=False)
+    user = models.OneToOneField(User, blank=True, null=True, help_text="Username for this person")
     
     def __unicode__(self):
         '''The unicode representation for a Personnel object is its full name'''
@@ -56,7 +60,10 @@ class Person(models.Model):
     
     def full_name(self):
         '''this function creates a full_name representation for both unicode and slug field displays.'''
-        return "%s %s" %(self.first_name, self.last_name)
+        if self.last_name is None:
+            return "No Name Given"
+        else:
+            return "%s %s" %(self.first_name, self.last_name)
     
     def save(self, *args, **kwargs):
         '''Over-rides save to generate name_slug field.  This is only set upon creation to keep stability.'''
@@ -68,6 +75,12 @@ class Person(models.Model):
         '''The meta options for Personnel models.'''
         ordering = ["last_name"]
         verbose_name_plural = "Personnel"
+        
+def create_user_profile(sender, instance, created, **kwargs):
+    '''This function creates a Person object for each user.'''
+    if created:
+        Person.objects.create(user=instance)
+post_save.connect(create_user_profile, sender=User)        
          
 class Role(models.Model):
     ''''This model describes the type of job a ::class`LabMember` had.
