dc28fae42fe4339e6769daeb94d829b0a8573978
Dave Bridges <dave.bridges@gmail.com>
2012-12-29 12:20:15 -0500

    Documented paper feed objects.  Part of issue #18 and issue #12.

--- papers/feeds.py
+++ papers/feeds.py
@@ -20,26 +20,34 @@ from django.conf import settings
 from papers.models import Publication
 
 class PapersFeed(Feed):
-    '''This is the main class for all feeds related to publications.'''
+    '''This is the main class for all feeds related to :class:`~papers.models.Publication`.'''
     
     def item_title(self, item):
+        '''The feed item title is the title of the paper.'''
         return item.title
 
     def item_description(self, item):
+        '''The feed description is the abstract of the paper.'''
         return item.abstract
 
 class LabPapersFeed(PapersFeed):
+    '''This generates a feed for papers from this group.'''
+
     title = "Papers From the %s" % settings.LAB_NAME
     link = "/feeds/lab-papers"
     description = "This feed contains papers published by members of the %s." % settings.LAB_NAME
 
     def items(self):
+        '''The items returned by this feed are papers set as laboratory papers.'''
         return Publication.objects.filter(laboratory_paper=True)
         
 class InterestingPapersFeed(PapersFeed):
+    '''This generates a feed for papers marked as interesting.'''
+
     title = "Papers of Interest"
     link = "/feeds/interesting-papers"
     description = "This feed contains papers that the %s is particularly interested in." % settings.LAB_NAME
 
     def items(self):
+        '''The items returned by this feed are papers set as interesting.'''
         return Publication.objects.filter(interesting_paper=True)
