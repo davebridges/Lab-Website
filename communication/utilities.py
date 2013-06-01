b42109a5f59b5dfaf1b3db8f8a5ceb826f2ecd3c
Dave Bridges <dave.bridges@gmail.com>
2013-05-31 23:17:03 -0500

    Added tweepy to handle twitter oauth requests

--- communication/utilities.py
+++ communication/utilities.py
@@ -4,10 +4,13 @@ This will include generic API request functionality (currently for oauth2).
 OAuth2 authentication is done using the python-oauth2 library (from https://github.com/brosner/python-oauth2).  This library in turn requires httplib2
 '''
 
-from django.conf import settings
-
 import oauth2 as oauth
 import time
+import tweepy
+
+from django.conf import settings
+
+
 
 def twitter_oauth_req(url, http_method="GET", post_body=None, http_headers=None):
     '''This function is a generic oauth request object.
@@ -28,3 +31,12 @@ def twitter_oauth_req(url, http_method="GET", post_body=None, http_headers=None)
         force_auth_header=True
     )
     return content
+    
+def twitter_oauth_tweepy():
+    '''This function sets up an API request.
+    
+    It returns a twitter API object, which can be queried later.'''
+    
+    twitter_authentication = tweepy.OAuthHandler(settings.TWITTER_CONSUMER_KEY, settings.TWITTER_CONSUMER_SECRET)
+    twitter_authentication.set_access_token(settings.TWITTER_ACCESS_TOKEN, settings.TWITTER_ACCESS_TOKEN_SECRET)
+    return tweepy.API(twitter_authentication)
