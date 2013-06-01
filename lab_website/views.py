f26fe0f8f39a2bd1532959d6648622a3dd8f09ec
Dave Bridges <dave.bridges@gmail.com>
2013-05-31 23:17:41 -0500

    Added tweets and papers to homepage

--- lab_website/views.py
+++ lab_website/views.py
@@ -4,9 +4,14 @@ So far this includes the home page.'''
 
 import json
 import urllib, urllib2
+import time
+import datetime
 
 from django.conf import settings
 from django.views.generic.base import View, TemplateView
+
+from papers.models import Publication
+from communication.utilities import twitter_oauth_tweepy
 
 
 class IndexView(TemplateView):
@@ -17,7 +22,11 @@ class IndexView(TemplateView):
     def get_context_data(self, **kwargs):
         '''This function provides the context which is passed to this view.
         
-        This will query facebook's pages API for simple information regarding the group.'''
+        This will query facebook's pages API for information regarding the group.
+        From facebook there will be separate queries for posts, photos, milestones and general information.
+        The Twitter API will be used to request facebook 
+        There will also be an internal query for lab publications.'''
+        
         context = super(IndexView, self).get_context_data(**kwargs)
         
         def facebook_request(request_url):
@@ -47,5 +56,11 @@ class IndexView(TemplateView):
         context['posts'] = facebook_request(posts_request_url)        
         photos_request_url = 'https://graph.facebook.com/' + settings.FACEBOOK_ALBUM +'/photos?access_token=' + settings.FACEBOOK_ACCESS_TOKEN + '&limit=10'
         context['photo_url'] = photos_request_url          
-        context['photos'] = facebook_request(photos_request_url)  
+        context['photos'] = facebook_request(photos_request_url)
+        
+        twitter_api = twitter_oauth_tweepy()
+        twitter_timeline = twitter_api.user_timeline(count=10)
+        context['tweets'] = twitter_timeline
+        
+        context['papers'] = Publication.objects.filter(laboratory_paper=True)
         return context                            
