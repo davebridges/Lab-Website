3172c21381327b40899c17cba456baf7c8c86556
Dave Bridges <dave.bridges@gmail.com>
2013-06-14 21:59:37 -0500

    Updated and passed communication tests, part of issue #14

--- communication/tests.py
+++ communication/tests.py
@@ -1,16 +1,84 @@
 """
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
+This file contains the unit tests for the :mod:`communication` app.
 
-Replace this with more appropriate tests for your application.
+Since this app has no models there is only view tests:
+
+* :class:`~communication.tests.FeedDetailViewTests` 
+
 """
 
-from django.test import TestCase
+from lab_website.tests import BasicTests
 
+class CommunicationViewTests(BasicTests):
+    '''This class tests the views associated with the :mod:`communication` app.'''
+    
+    
+    def test_feed_details_view(self):
+        """This tests the feed-details view, ensuring that templates are loaded correctly.  
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+        This view uses a user with superuser permissions so does not test the permission levels for this view."""
+        
+        test_response = self.client.get('/feeds')
+        self.assertEqual(test_response.status_code, 200)       
+        self.assertTemplateUsed(test_response, 'feed_details.html')
+        self.assertTemplateUsed(test_response, 'base.html') 
+        self.assertTemplateUsed(test_response, 'twitter_anywhere_script.html')
+        self.assertTemplateUsed(test_response, 'jquery_script.html') 
+        self.assertTrue('google_calendar_id' in test_response.context)          
+        
+    def test_lab_rules_view(self):
+        '''This tests the lab-rules view.
+        
+        The tests ensure that the correct template is used.
+        It also tests whether the correct context is passed (if included).
+        his view uses a user with superuser permissions so does not test the permission levels for this view.'''
+        
+        test_response = self.client.get('/communication/lab-rules')
+        self.assertEqual(test_response.status_code, 200)       
+        self.assertTemplateUsed(test_response, 'lab_rules.html')
+        self.assertTemplateUsed(test_response, 'base.html') 
+        self.assertTemplateUsed(test_response, 'twitter_anywhere_script.html')
+        self.assertTemplateUsed(test_response, 'jquery_script.html') 
+        self.assertTrue('lab_rules' in test_response.context)    
+        self.assertTrue('lab_rules_source' in test_response.context)    
+        
+    def test_twitter_view(self):
+        '''This tests the twitter view.
+        
+        Currently it just ensures that the template is loading correctly.
+        '''
+        test_response = self.client.get('/communication/twitter')
+        self.assertEqual(test_response.status_code, 200)       
+        self.assertTemplateUsed(test_response, 'twitter_timeline.html')
+        self.assertTemplateUsed(test_response, 'base.html') 
+        self.assertTemplateUsed(test_response, 'twitter_anywhere_script.html')
+        self.assertTemplateUsed(test_response, 'jquery_script.html') 
+        self.assertTrue('timeline' in test_response.context)         
+        
+    def test_calendar_view(self):
+        '''This tests the google-calendar view.
+        
+        Currently it just ensures that the template is loading correctly.
+        '''
+        test_response = self.client.get('/communication/calendar')
+        self.assertEqual(test_response.status_code, 200)       
+        self.assertTemplateUsed(test_response, 'calendar.html')
+        self.assertTemplateUsed(test_response, 'base.html') 
+        self.assertTemplateUsed(test_response, 'twitter_anywhere_script.html')
+        self.assertTemplateUsed(test_response, 'jquery_script.html') 
+        self.assertTrue('google_calendar_id' in test_response.context)                    
+                                
+        
+    def test_wikipedia_view(self):
+        '''This tests the google-calendar view.
+        
+        Currently it just ensures that the template is loading correctly.
+        '''
+        test_response = self.client.get('/communication/wikipedia')
+        self.assertEqual(test_response.status_code, 200)       
+        self.assertTemplateUsed(test_response, 'wikipedia_edits.html')
+        self.assertTemplateUsed(test_response, 'base.html') 
+        self.assertTemplateUsed(test_response, 'twitter_anywhere_script.html')
+        self.assertTemplateUsed(test_response, 'jquery_script.html') 
+        self.assertTrue('google_calendar_id' in test_response.context)                                 
+                                
--- communication/urls.py
+++ communication/urls.py
@@ -1,4 +1,4 @@
-'''This package has the url encodings for the urls app.'''
+'''This package has the url encodings for the communication app.'''
 
 from django.conf.urls import patterns, include, url
 
@@ -9,17 +9,8 @@ from communication import views
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'lab_website.views.home', name='home'),
-    # url(r'^lab_website/', include('lab_website.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-    url(r'^twitter/$', views.TwitterView.as_view(), name="twitter"),
-    url(r'^calendar/$', views.GoogleCalendarView.as_view(), name="google-calendar"),
-    url(r'^wikipedia/$', views.WikipedaEditsView.as_view(), name="wikipedia"),
-    url(r'^lab-rules/$', views.LabRulesView.as_view(), name="lab-rules") 
+    url(r'^twitter/?$', views.TwitterView.as_view(), name="twitter"),
+    url(r'^calendar/?$', views.GoogleCalendarView.as_view(), name="google-calendar"),
+    url(r'^wikipedia/?$', views.WikipedaEditsView.as_view(), name="wikipedia"),
+    url(r'^lab-rules/?$', views.LabRulesView.as_view(), name="lab-rules") 
 )
