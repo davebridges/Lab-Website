59e4fc78667ef9e66e6dd24b6a6d5df860865589
Dave Bridges <dave.bridges@gmail.com>
2013-06-14 21:03:53 -0500

    Wrote base test class in root folder

---
+++ lab_website/tests.py
@@ -0,0 +1,33 @@
+"""
+This file contains the basic unit tests, which are imported into other apps.
+
+
+"""
+
+from django.test import TestCase
+from django.test.client import Client
+from django.contrib.auth.models import User
+
+from papers.models import Publication
+
+MODELS = [Publication,]
+
+class BasicTests(TestCase):
+    '''This class covers the setup and tear down for all unit tests'''
+
+    def setUp(self):
+        """Instantiate the test client.  Creates a test user."""
+        self.client = Client()
+        self.test_user = User.objects.create_user('testuser', 'blah@blah.com', 'testpassword')
+        self.test_user.is_superuser = True
+        self.test_user.is_active = True
+        self.test_user.save()
+        self.assertEqual(self.test_user.is_superuser, True)
+        login = self.client.login(username='testuser', password='testpassword')
+        self.failUnless(login, 'Could not log in')
+
+    def tearDown(self):
+        """Depopulate created model instances from test database."""
+        for model in MODELS:
+            for obj in model.objects.all():
+                obj.delete()
