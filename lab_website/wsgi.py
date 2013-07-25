73eab5f708930fc06d4d50ed864b843f330c6d34
Dave Bridges <dave.bridges@gmail.com>
2013-07-25 14:19:56 -0500

    mend
    Removed email and phone from personnel snippet.

--- lab_website/wsgi.py
+++ lab_website/wsgi.py
@@ -26,3 +26,6 @@ application = get_wsgi_application()
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
 # application = HelloWorldApplication(application)
+
+import sys
+sys.path.append('/home/dbridge9/src/Lab-Website')
