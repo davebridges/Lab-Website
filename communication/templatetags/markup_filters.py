c3369b4132f96ee277dd3c2c308afa6e83af3b0a
Dave Bridges <dave.bridges@gmail.com>
2013-07-28 08:29:27 -0500

    Wrote new RST template filter, since the Django markup file is being deprecated.

---
+++ communication/templatetags/markup_filters.py
@@ -0,0 +1,16 @@
+'''This package has the template filters for the communication app.
+
+Currently there is just a markdown filter for converting RST into HTML files.'''
+
+from docutils.core import publish_parts
+from django import template
+from django.template.defaultfilters import stringfilter
+
+register = template.Library()
+
+@register.filter(is_safe=True)
+@stringfilter
+def markdown_rst(value):
+    return publish_parts(value, writer_name='html')['html_body']
+
+register.filter('markdown_rst', markdown_rst)
