d7d2e7126f8aca4c76bf35684437a96f64334fdc
Dave Bridges <dave.bridges@gmail.com>
2013-11-28 07:10:49 -0600

    Filtered AuthorDetails on Publication Form to only show un-assigned authors.

--- papers/forms.py
+++ papers/forms.py
@@ -8,8 +8,10 @@ from papers.models import Publication, AuthorDetails
 class PublicationForm(forms.ModelForm):
     '''This class generates the forms for creating and editing publications.
 
+    This form only shows authors which have been pre-ordered but not yet assigned to a publication.
+
     '''
-    authors = forms.ModelMultipleChoiceField(queryset=AuthorDetails.objects.all(),
+    authors = forms.ModelMultipleChoiceField(queryset=AuthorDetails.objects.filter(publication__isnull=True),
         widget=forms.CheckboxSelectMultiple)
 
     class Meta:
