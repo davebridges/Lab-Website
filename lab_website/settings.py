2046de123cfb6c2cc17f60a0808fe53cf5131de5
Dave Bridges <dave.bridges@gmail.com>
2016-06-02 12:31:26 -0400

    removed debug toolbar, part of issue #25

--- lab_website/settings.py
+++ lab_website/settings.py
@@ -32,7 +32,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware'
+    #'debug_toolbar.middleware.DebugToolbarMiddleware'
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
