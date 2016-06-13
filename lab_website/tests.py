7b6fa8b87bfcc33ebe2ecdc539de18b1e15c7cbb
Dave Bridges <dave.bridges@gmail.com>
2016-06-13 15:13:13 -0400

    Wrote tests for home view

--- lab_website/tests.py
+++ lab_website/tests.py
@@ -33,3 +33,32 @@ class BasicTests(TestCase):
         for model in MODELS:
             for obj in model.objects.all():
                 obj.delete()
+                
+class HomeViewTests(BasicTests):
+    '''This class tests the views associated with the :mod:`communication` app.'''
+    
+    
+    def test_feed_details_view(self):
+        """This tests the feed-details view, ensuring that templates are loaded correctly.  
+
+        This view uses a user with superuser permissions so does not test the permission levels for this view."""
+        
+        test_response = self.client.get('/')
+        self.assertEqual(test_response.status_code, 200)       
+        self.assertTemplateUsed(test_response, 'index.html')
+        self.assertTemplateUsed(test_response, 'base.html') 
+        self.assertTemplateUsed(test_response, 'jquery_script.html') 
+        self.assertTemplateUsed(test_response, 'facebook_api_sdk_script.html') 
+        self.assertTemplateUsed(test_response, 'job_posting_snippet.html') 
+        self.assertTemplateUsed(test_response, 'analytics_tracking.html') 
+         #check that context processors are being passed                       
+        self.assertTrue('lab_name' in test_response.context)
+        self.assertTrue('twitter' in test_response.context)
+        self.assertTrue('google_plus' in test_response.context)
+        self.assertTrue('facebook' in test_response.context)
+        self.assertTrue('disqus_forum' in test_response.context)        
+        self.assertTrue('fb_app_id' in test_response.context)
+        self.assertTrue('fb_admins' in test_response.context)
+        self.assertTrue('analytics_tracking' in test_response.context)        
+        self.assertTrue('analytics_root' in test_response.context)                          
+
