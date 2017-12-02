14e6f7d016ea7bedaeb7647aa189ddb52dff0bb3
Dave Bridges <dave.bridges@gmail.com>
2017-12-01 22:55:00 -0500

    Altered from cover photo to main photo

--- lab_website/views.py
+++ lab_website/views.py
@@ -47,7 +47,7 @@ class IndexView(TemplateView):
                      data = json.loads(json_data)
                      return data
          
-        general_request_url = 'https://graph.facebook.com/v2.11/' + settings.FACEBOOK_ID + '?access_token=' + settings.FACEBOOK_ACCESS_TOKEN + '&fields=description,about,cover'
+        general_request_url = 'https://graph.facebook.com/v2.11/' + settings.FACEBOOK_ID + '?access_token=' + settings.FACEBOOK_ACCESS_TOKEN + '&fields=description,about,cover,photos{webp_images}'
         context['recent_papers'] =  Publication.objects.filter(laboratory_paper=True)[0:10]  
         context['recent_posts'] =  Post.objects.all()[0:5]  
         context['recent_comments'] =  Commentary.objects.all()[0:5]                 
