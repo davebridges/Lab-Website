23040444b816d9a5c7d9e8550611157c007e34f4
Dave Bridges <dave.bridges@gmail.com>
2017-12-01 22:26:07 -0500

    Added cover image to homepage.

--- lab_website/views.py
+++ lab_website/views.py
@@ -47,7 +47,7 @@ class IndexView(TemplateView):
                      data = json.loads(json_data)
                      return data
          
-        general_request_url = 'https://graph.facebook.com/v2.3/' + settings.FACEBOOK_ID + '?access_token=' + settings.FACEBOOK_ACCESS_TOKEN + '&fields=description,about'
+        general_request_url = 'https://graph.facebook.com/v2.11/' + settings.FACEBOOK_ID + '?access_token=' + settings.FACEBOOK_ACCESS_TOKEN + '&fields=description,about,cover'
         context['recent_papers'] =  Publication.objects.filter(laboratory_paper=True)[0:10]  
         context['recent_posts'] =  Post.objects.all()[0:5]  
         context['recent_comments'] =  Commentary.objects.all()[0:5]                 
