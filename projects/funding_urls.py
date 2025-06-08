fa86a4f5d32659e47fecb37db5223da85aec811d
Dave Bridges <dave.bridges@gmail.com>
2025-06-08 14:46:27 -0400

    Upgraded urls for funding_urls, part of issue #40

--- projects/funding_urls.py
+++ projects/funding_urls.py
@@ -1,13 +1,13 @@
-'''This package has the url encodings for the :mod:`funding` model.'''
+"""URL patterns for the funding model in the projects app."""
 
-from django.conf.urls import include, url
+from django.urls import path, re_path
 
 from projects import views
 
 urlpatterns = [
-    url(r'^new/?$', views.FundingCreate.as_view(), name="funding-new"),     
-    url(r'^(?P<title_slug>[-\w\d]+)/edit/?$', views.FundingUpdate.as_view(), name="funding-edit"),  
-    url(r'^(?P<title_slug>[-\w\d]+)/delete/?$', views.FundingDelete.as_view(), name="funding-delete"),    
-    url(r'^(?P<title_slug>[-\w\d]+)/?$', views.FundingDetailView.as_view(), name="funding-details"),                    
-    url(r'^$', views.FundingList.as_view(), name="funding-list"), 
+    path('new/', views.FundingCreate.as_view(), name="funding-new"),
+    re_path(r'^(?P<title_slug>[-\w\d]+)/edit/$', views.FundingUpdate.as_view(), name="funding-edit"),
+    re_path(r'^(?P<title_slug>[-\w\d]+)/delete/$', views.FundingDelete.as_view(), name="funding-delete"),
+    re_path(r'^(?P<title_slug>[-\w\d]+)/$', views.FundingDetailView.as_view(), name="funding-details"),
+    path('', views.FundingList.as_view(), name="funding-list"),
 ]
